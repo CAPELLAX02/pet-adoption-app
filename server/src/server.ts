e0011e36d5c9456eb2c2d65ee1338bd4c10abbd6
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-14 01:56:51 +0300

    register and logic routes implemented successfully.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { connectDB } from './config/database';
+import userRouter from './routes/userRoutes';
+
 require('dotenv').config();
 const PORT = process.env.PORT || 8001;
 
@@ -10,8 +12,11 @@ const app: Express = express();
 connectDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
+
+app.use('/api/users', userRouter);
 
 app.get('/test', (req: Request, res: Response, next: NextFunction) => {
   res.send('Test API is working.');
